9fad73c3faf50049d5c084b785877fd85a2b7ab1
bababooey-y <maximegeffroy24@gmail.com>
2026-02-04 19:26:09 +0100

    change

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,6 @@
 from flask import request, render_template, Blueprint
 from werkzeug.security import check_password_hash, generate_password_hash
+from src.db import USERS
 
 auth_blueprint = Blueprint("auth",__name__)
 
@@ -14,5 +15,22 @@ def post_register():
     username = form.get("username")
     password = form.get("password")
     secure_password = generate_password_hash(password)
-    print(username, password, secure_password)
-    return
+    if username not in USERS.keys():
+        USERS[username] = secure_password
+        return redirect(url_for("auth.get_login"))
+
+    print("utilisateur non ajouté")
+    return redirect(url_for("auth.get_login"))
+
+@auth_blueprint.get("/login")
+def get_login():
+    return render_templates("login.html")
+
+@auth_blueprint.post("/login")
+def post_login():
+    form = request.form.to_dict()
+    username = form.get("username")
+    password_hash = form.get("password")
+
+
+
